fdca3c266b4a9d7cb49821cba564cd364047e4d4
ahmetemre0 <buz20@itu.edu.tr>
2024-06-04 22:15:58 +0300

    chore: Add initial server setup code

---
+++ app.js
@@ -0,0 +1,33 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const mongoose = require('mongoose');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(8000, () => {
+    console.log('Server is running on port 8000');
+});
+
+const ListRouter = require('./src/route/List');
+const WordRouter = require('./src/route/Word');
+const QuizRouter = require('./src/route/quiz');
+
+
+app.use('/api/v1/list', ListRouter);
+app.use('/api/v1/word', WordRouter);
+app.use('/api/v1/quiz', QuizRouter);
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to MongoDB');
+});
+
+mongoose.connection.on('error', (error) => {
+    console.log('Error:', error);
+});
